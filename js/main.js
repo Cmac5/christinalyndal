1145c139def1485c8b0eced558274a53ab56856e
Christina <christinamacdonald5@gmail.com>
2018-06-18 17:50:00 +0800

    pages fixed

--- js/main.js
+++ js/main.js
@@ -6,10 +6,31 @@ In this way Javascript will only be fired once the DOM is loaded
 $(document).ready(function() {
 
   // JavaScript
-window.sr = ScrollReveal();
+  window.sr = ScrollReveal();
 
-// Customizing a reveal set
-sr.reveal('.show-me', { duration: 2000 });
+  // Customizing a reveal set
+  sr.reveal('.show-me', { duration: 2000 });
 
+
+  var scrollTop = 0;
+  $(window).scroll(function(){
+    scrollTop = $(window).scrollTop();
+     $('.counter').html(scrollTop);
+
+    if (scrollTop >= 250) {
+      $('header').addClass('scrolled-nav');
+    } else if (scrollTop < 250) {
+      $('header').removeClass('scrolled-nav');
+    }
+
+  });
+
+  $('#up, #down').on('click', function(e){
+      e.preventDefault();
+      var target= $(this).get(0).id == 'up' ? $('#down') : $('#up');
+      $('html, body').stop().animate({
+         scrollTop: target.offset().top
+      }, 1000);
+  });
 
 });
