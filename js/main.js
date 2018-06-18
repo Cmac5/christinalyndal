86aa88fd3f8d022de68cbd858ce5af7d1edf386a
Christina <christinamacdonald5@gmail.com>
2018-06-18 21:11:51 +0800

    Remove showme js

--- js/main.js
+++ js/main.js
@@ -5,13 +5,15 @@ In this way Javascript will only be fired once the DOM is loaded
 
 $(document).ready(function() {
 
-  // JavaScript
-  window.sr = ScrollReveal();
+  // SHOW ME
+  // // JavaScript
+  // window.sr = ScrollReveal();
+  //
+  // // Customizing a reveal set
+  // sr.reveal('.show-me', { duration: 2000 });
 
-  // Customizing a reveal set
-  sr.reveal('.show-me', { duration: 2000 });
 
-
+  // STICKY NAV
   var scrollTop = 0;
   $(window).scroll(function(){
     scrollTop = $(window).scrollTop();
@@ -25,6 +27,7 @@ $(document).ready(function() {
 
   });
 
+  // TINT ARROW
   $('#up, #down').on('click', function(e){
       e.preventDefault();
       var target= $(this).get(0).id == 'up' ? $('#down') : $('#up');
